cd99de6455d17231b72b33326bd34861bca0ec0a
briellaw <x.jingyii@gmail.com>
2026-06-09 23:25:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -97,6 +97,11 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "a") x -= 10;
   if (e.key === "d") x += 10;
 
+  if (e.key === "ArrowUp") y -= 10;
+  if (e.key === "ArrowDown") y += 10;
+  if (e.key === "ArrowLeft") x -= 10;
+  if (e.key === "ArrowRight") x += 10;
+
   player.style.left = x + "px";
   player.style.top = y + "px";
 
